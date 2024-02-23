91c371592c94adf303478e01874a64ca63510eb5
wall7Ddev <104283916+Wall7Dev@users.noreply.github.com>
2024-02-23 12:24:12 -0800

    Delete components directory

--- components/images2.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from "next";
-import styles from "./images2.module.css";
-
-const Images2: NextPage = () => {
-  return <div className={styles.images21} />;
-};
-
-export default Images2;
--- components/vector.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from "next";
-import styles from "./vector.module.css";
-
-const Vector: NextPage = () => {
-  return <div className={styles.vectorDiv} />;
-};
-
-export default Vector;
